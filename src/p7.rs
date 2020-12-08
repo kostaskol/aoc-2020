e60c98fb9221879a2819b55b18712e70c8e69686
Kostaskol <kwstaskolivas@gmail.com>
2020-12-08 20:39:47 +0200

    Remove outdated comments and debug msgs from p7

--- src/p7.rs
+++ src/p7.rs
@@ -1,4 +1,5 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
+use std::str::FromStr;
 use regex::Regex;
 
 use crate::utils;
@@ -15,73 +16,44 @@ pub fn run(extra_star: bool) {
   println!("{}", total);
 }
 
-fn count_shiny_gold(bags: &HashSet<Bag>) -> u16 {
-  // let mut targets = vec!["shiny gold bag"];
-  // let mut total = 0u16;
-  // let mut visited: HashSet<String> = HashSet::new();
-
-  // // Hacky and awful but actually gives us the bag we want
-  // // Please fix :(
-  // let mut targets = vec![bags.get(&Bag { color: "shiny gold bag".to_string(), contents: None})];
-
-  // println!("{:?}", bags);
-  // println!("{:?}", targets);
-
-  count_shiny_helper("shiny gold bag", bags) - 1
-
-
-  // while let Some(curr_target) = targets.pop() {
-  //   let curr_target = curr_target.unwrap();
-  //   let contents = curr_target.contents;
-  //   if contents.is_none() {
-  //     continue;
-  //   }
-
-  //   total += 
-  // }
-  // total
+fn count_shiny_gold(bags: &HashMap<String, Bag>) -> u16 {
+  let mut known: HashMap<String, u16> = HashMap::new();
+  count_shiny_helper("shiny gold bag", bags, &mut known) - 1
 }
 
-fn count_shiny_helper(target: &str, bags: &HashSet<Bag>) -> u16 {
-  let target_bag = bags.get(&Bag { color: String::from(target), contents: None });
+fn count_shiny_helper(target: &str, bags: &HashMap<String, Bag>, known: &mut HashMap<String, u16>) -> u16 {
+  let curr_target = bags.get(target).unwrap();
 
-  println!("Target: {:?}", target_bag);
-  match &target_bag.unwrap().contents {
+  match &curr_target.contents {
     Some(contents) => {
       let mut overall = 0u16;
 
-      for bag in contents {
-        let h = count_shiny_helper(&bag.name, bags);
-        println!("{} * {} ({})", bag.num, bag.name, h);
-        overall += bag.num as u16 * h
+      for content in contents {
+        if let Some(cap) = known.get(&content.name) {
+          overall += content.num as u16 * cap;
+        } else {
+          let h = count_shiny_helper(&content.name, bags, known);
+          known.insert(content.name.clone(), h);
+          overall += content.num as u16 * h
+        }
       }
-
-      println!("{} returning {}", target, overall);
       overall + 1
     },
-    None => {
-      println!("{} returning 1", target);
-      1
-    }
+    None => 1
   }
 }
 
-fn find_shiny_gold(bags: &HashSet<Bag>) -> u16{
+fn find_shiny_gold(bags: &HashMap<String, Bag>) -> u16{
   let mut targets = vec!["shiny gold bag"];
   let mut total = 0u16;
 
   let mut visited: HashSet<String> = HashSet::new();
 
   while let Some(curr_target) = targets.pop() {
-    println!("Current target: {}", curr_target);
-
-    for bag in bags {
-      println!("Bag: {:?}", bag);
-
-      if bag.contains(&curr_target).is_some() && !visited.contains(&bag.color) {
-        println!("It's a match!!");
-        visited.insert(bag.color.clone());
-        targets.push(&bag.color);
+    for (color, bag) in bags {
+      if bag.contains(&curr_target).is_some() && !visited.contains(color) {
+        visited.insert(color.clone());
+        targets.push(&color);
         total += 1;
       }
     }
@@ -90,50 +62,8 @@ fn find_shiny_gold(bags: &HashSet<Bag>) -> u16{
   total
 }
 
-#[derive(Debug)]
-struct Bag {
-  color: String,
-  contents: Option<Vec<Content>>
-}
-
-#[derive(Debug)]
-struct Content {
-  num: u8,
-  name: String
-}
-
-
-impl PartialEq for Bag {
-  fn eq(&self, other: &Self) -> bool {
-    self.color == other.color
-  }
-}
-
-impl std::hash::Hash for Bag {
-  fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-    self.color.hash(state);
-  }
-}
-
-impl Eq for Bag {}
-
-
-impl Bag {
-  fn contains(&self, cont: &str) -> Option<u8> {
-    if let Some(contents) = &self.contents {
-      for bag in contents {
-        if bag.name == cont {
-          return Some(bag.num);
-        }
-      }
-    }
-
-    None
-  }
-}
-
-fn parse_bags(lines: &[String]) -> HashSet<Bag> {
-  let mut bags: HashSet<Bag> = HashSet::new();
+fn parse_bags(lines: &[String]) -> HashMap<String, Bag> {
+  let mut bags: HashMap<String, Bag> = HashMap::new();
 
   for line in lines {
     let init_parts: Vec<&str> = line.split(" contain ").collect();
@@ -141,7 +71,7 @@ fn parse_bags(lines: &[String]) -> HashSet<Bag> {
     let bag_name = init_parts[0].replace("bags", "bag");
     let contents = parse_contents(init_parts[1]);
 
-    bags.insert(Bag { color: bag_name.to_string(), contents });
+    bags.insert(bag_name.to_string(), Bag::new(contents));
   }
 
   bags
@@ -156,18 +86,57 @@ fn parse_contents(content_parts: &str) -> Option<Vec<Content>> {
   let replaced = content_parts.replace('.', "").replace("bags", "bag");
   let content_parts: Vec<&str> = replaced.split(", ").collect();
 
-  let re = Regex::new(r"^(\d+)\s+(.*)$").unwrap();
-
   for part in content_parts {
-    match re.captures(part) {
-      Some(x) => {
-        let num = x.get(1).unwrap().as_str().parse().unwrap();
-        let name = x.get(2).unwrap().as_str();
-        contents.push(Content { num, name: String::from(name) });
-      },
-      None => panic!("could not match regex for {}", part)
-    }
+    contents.push(Content::from_str(part).unwrap());
   }
 
   Some(contents)
+}
+
+#[derive(Debug)]
+struct Bag {
+  contents: Option<Vec<Content>>
+}
+
+#[derive(Debug)]
+struct Content {
+  num: u8,
+  name: String
+}
+
+impl Bag {
+  fn new(contents: Option<Vec<Content>>) -> Self {
+    Self {
+      contents
+    }
+  }
+
+  fn contains(&self, cont: &str) -> Option<u8> {
+    if let Some(contents) = &self.contents {
+      for bag in contents {
+        if bag.name == cont {
+          return Some(bag.num);
+        }
+      }
+    }
+
+    None
+  }
+}
+
+impl FromStr for Content {
+  type Err = String;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    let re = Regex::new(r"^(\d+)\s+(.*)$").unwrap();
+
+    match re.captures(s) {
+      Some(x) => {
+        let num = x.get(1).unwrap().as_str().parse().unwrap();
+        let name = x.get(2).unwrap().as_str();
+        return Ok(Self { num, name: String::from(name) })
+      },
+      None => Err(format!("could not match regex for {}", s))
+    }
+  }
 }
